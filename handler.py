d9b29bff2f7fe3490f3c821b31e22db1ba852e4a
Jeremias <moraesjeremias@Jeremiass-MacBook-Pro.local>
2020-10-27 01:39:49 -0300

    env variables for Rekognition model

--- handler.py
+++ handler.py
@@ -5,11 +5,12 @@ import os
 load_dotenv()
 ACCESS_KEY = os.environ.get("ACCESS_KEY")
 SECRET_KEY = os.environ.get("SECRET_KEY")
+REKOGNITION_MODEL = os.environ.get("REKOGNITION_MODEL")
 
 rekognition = boto3.client('rekognition', region_name='us-east-1', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
 
 def detect_custom_labels(bucket, key):    
-    response = rekognition.detect_custom_labels(ProjectVersionArn='arn:aws:rekognition:us-east-1:018413147376:project/carne-moida/version/carne-moida.2020-10-09T00.57.23/1602215843140',Image={'S3Object': {'Bucket': bucket,'Name': key,}},MaxResults=10,MinConfidence=0.5)
+    response = rekognition.detect_custom_labels(ProjectVersionArn=REKOGNITION_MODEL, Image={'S3Object': {'Bucket': bucket,'Name': key,}}, MaxResults=10, MinConfidence=30)
     return response
 
 def lambda_handler(event, context):
