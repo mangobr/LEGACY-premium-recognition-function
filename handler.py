b8cba540da1780b44c625b16417a6a1bcbaefcbc
Christian <christian.trindade94@gmail.com>
2020-10-30 13:38:49 -0300

    add .serverless on gitignore, remove fixed path to bucked and image, remove comment on dotenv

--- handler.py
+++ handler.py
@@ -1,8 +1,7 @@
 import boto3
 import urllib
-#from dotenv import load_dotenv
 import os
-#load_dotenv()
+
 ACCESS_KEY = os.environ.get("ACCESS_KEY")
 SECRET_KEY = os.environ.get("SECRET_KEY")
 REKOGNITION_MODEL = os.environ.get("REKOGNITION_MODEL")
@@ -14,10 +13,9 @@ def detect_custom_labels(bucket, key):
     return response
 
 def lambda_handler(event, context):
-    # bucket = event['Records'][0]['s3']['bucket']['name']
-    # key = urllib.unquote_plus(event['Records'][0]['s3']['object']['key'].encode('utf8'))
-    bucket = 'com.rekoreko.premium'
-    key = 'perfil.png'
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.unquote_plus(event['Records'][0]['s3']['object']['key'].encode('utf8'))
+
     try:
         lambda_handler_response = detect_custom_labels(bucket, key)
         return lambda_handler_response
