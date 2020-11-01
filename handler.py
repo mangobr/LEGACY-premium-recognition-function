aba52c3fb722834ce69f9323595edef6bf83e7d6
Jeremias <moraesjeremias@Jeremiass-MacBook-Pro.local>
2020-11-01 00:33:06 -0300

    removendo secrets

--- handler.py
+++ handler.py
@@ -1,12 +1,9 @@
 import boto3
 import urllib
 import os
+REKOGNITION_MODEL = os.environ.get('REKOGNITION_MODEL')
 
-ACCESS_KEY = os.environ.get("ACCESS_KEY")
-SECRET_KEY = os.environ.get("SECRET_KEY")
-REKOGNITION_MODEL = os.environ.get("REKOGNITION_MODEL")
-
-rekognition = boto3.client('rekognition', region_name='us-east-1', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
+rekognition = boto3.client('rekognition', region_name='us-east-1')
 
 def detect_custom_labels(bucket, key):    
     response = rekognition.detect_custom_labels(ProjectVersionArn=REKOGNITION_MODEL, Image={'S3Object': {'Bucket': bucket,'Name': key,}}, MaxResults=10, MinConfidence=30)
